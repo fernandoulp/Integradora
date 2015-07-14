de7ccf9177a59c8e3fe63e860c9b87827d8262a6
fernandoulp <fernandoulp@gmail.com>
2015-07-14 14:46:08 -0500

    código para bases de datos
    Todavía no estoy seguro si puede insertar datos a bases de datos; base
    de datos que hice se llama “prueba” con los campos: nombre, email,
    comentario.

--- contac.php
+++ contac.php
@@ -8,8 +8,16 @@ $para = 'consultic9@gmail.com';
 $titulo = 'Mensaje enviado desde contáctanos en consultic.bravoutd.com';
 $header = 'From: ' . $email;
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
-  
-if ($_POST['submit']) {
+
+// Variables para conexión a base de datos de pruebas
+$hostname = 'localhost';
+$dbname = 'pruebas';
+$username = 'root':
+$password = '';
+// --->  
+if ($_POST['submit']) 
+{
+
 if (mail($para, $titulo, $msjCorreo, $header)) {
 echo "<script language='javascript'>
 alert('Mensaje enviado, responderemos a la brevedad...Gracias!.');
@@ -18,6 +26,22 @@ window.location.href = 'http://google.com';
 } else {
 echo 'Falló el envio, intenta de nuevo porfavor.';
 }
+
+
+// Intento para conexión a bases de datos e inserción
+try {
+  $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $stmt = $dbh->prepare("INSERT INTO contacto(nombre, email, mensaje) VALUES (:nombre, :email, :mensaje)");
+
+
+
+  }
+
+
+
 }
 
 ?>
