72a11ed6a33b4f986aece02028fc916e76782cb0
fernandoulp <fernandoulp@gmail.com>
2015-07-16 14:53:30 -0500

    carrusel para personal
    carrusel en la sección de quiénes somos, voy a poner fotos de nosotros,
    con nuestros roles y una pequeña descripción

--- contac.php
+++ contac.php
@@ -9,12 +9,7 @@ $titulo = 'Mensaje enviado desde contáctanos en consultic.bravoutd.com';
 $header = 'From: ' . $email;
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
 
-// Variables para conexión a base de datos de pruebas
-$hostname = 'localhost';
-$dbname = 'pruebas';
-$username = 'root':
-$password = '';
-// --->  
+
 if ($_POST['submit']) 
 {
 
@@ -28,18 +23,7 @@ echo 'Falló el envio, intenta de nuevo porfavor.';
 }
 
 
-// Intento para conexión a bases de datos e inserción
-try {
-  $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
 
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  $stmt = $dbh->prepare("INSERT INTO contacto(nombre, email, mensaje) VALUES (:nombre, :email, :mensaje)");
-
-
-
-  }
-// -->
 
 
 
